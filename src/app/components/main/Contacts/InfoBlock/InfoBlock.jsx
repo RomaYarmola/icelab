1c326ccc4f39abd5ec62a28c3f5afa6415d74e1f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-07-07 19:27:51 +0300

    updated contacts

--- src/app/components/main/Contacts/InfoBlock/InfoBlock.jsx
+++ src/app/components/main/Contacts/InfoBlock/InfoBlock.jsx
@@ -1,6 +1,7 @@
 import Container from "@/utils/Container";
 import Form from "./Form";
 import ContactsBlock from "./ContactsBlock";
+import Image from "next/image";
 
 export default function InfoBlock() {
   return (
@@ -8,8 +9,27 @@ export default function InfoBlock() {
       <div className="absolute radial-white-gradient w-[2447px] max-h-[375px] top-[-29px] left-1/2 transform -translate-x-1/2 z-[1]" />
       <Container>
         <div className="pt-[27px] md:pt-32 flex flex-col md:flex-row gap-20 md:gap-4 md:justify-between pb-[122px] l:pb-[148px] relative z-10">
-          <Form />
+          <div className="">
+            {" "}
+            <Form />
+            <div className="hidden md:block relative w-full mt-11 rounded-[14px] overflow-hidden overflow-hidden h-[200px] md:h-[234px]">
+              <Image
+                src="/images/contacts/contacts.webp"
+                alt="contacts"
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
           <ContactsBlock />
+          <div className="md:hidden relative w-full rounded-[14px] overflow-hidden overflow-hidden h-[200px] md:h-[234px]">
+            <Image
+              src="/images/contacts/contacts.webp"
+              alt="contacts"
+              fill
+              className="object-cover"
+            />
+          </div>
         </div>
       </Container>
     </div>
